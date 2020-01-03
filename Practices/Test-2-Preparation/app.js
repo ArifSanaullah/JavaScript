67cdeb5a9439e6a194302987bffd805b2aa6b0e5
ArifSanaullah <ak7896023@gmail.com>
2020-01-03 14:36:58 +0500

    JS Quiz 2 preparation

---
+++ Practices/Test-2-Preparation/app.js
@@ -0,0 +1,20 @@
+// var checkCity = prompt("Enter your city: ");
+// checkCity = checkCity.toLowerCase();
+// var cleanestCities = [
+//   "cheyenne",
+//   "santa fe",
+//   "tucson",
+//   "great falls",
+//   "honolulu"
+// ];
+
+// for (var i = 0; i < cleanestCities.length; i++) {
+//     if (checkCity === cleanestCities[i]) {
+//         console.log("City is the cleanest.")
+//     }
+// }
+// console.log(cleanestCities[2].toLocaleUpperCase())
+
+var word = "Boston";
+var someLet = word.slice(1, 3);
+console.log(someLet);

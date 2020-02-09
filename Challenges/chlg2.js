3967e4e1abbc446a459bfe2ae34649dfed4f19d5
ArifSanaullah <ak7896023@gmail.com>
2020-02-10 02:20:12 +0500

    Challenge 2

--- Challenges/chlg2.js
+++ Challenges/chlg2.js
@@ -22,27 +22,79 @@
 // }
 // WAFT(1278452345356345345);
 
-const mumbler = function (str) {
-    let result = [...str];
-    let foo = [];
-    for (let i = 0; i < result.length; i++) {
-        const element = result[i];
-        for (let j = 0; j < i+1; j++) {
-            foo.push(element);
-        }
-    }
+// const mumbler = function (str) {
+//     let result = [...str];
+//     let foo = [];
+//     for (let i = 0; i < result.length; i++) {
+//         const element = result[i];
+//         for (let j = 0; j < i+1; j++) {
+//             foo.push(element);
+//         }
+//     }
 
-    result = [];
-    for (let i = 0; i < foo.length; i++) {
-        let letter = foo[i];
-        const element = foo[i];
-        if (letter[letter.length-1] === element) {
-            letter = element;
-        }
-        result.push(letter);
-    }
-    foo = foo.toString().replace(/,/g, "");
-    console.log(result);
-    console.log(foo);
+//     result = [];
+//     for (let i = 0; i < foo.length; i++) {
+//         let letter = foo[i];
+//         const element = foo[i];
+//         if (letter[letter.length-1] === element) {
+//             letter = element;
+//         }
+//         result.push(letter);
+//     }
+//     foo = foo.toString().replace(/,/g, "");
+//     console.log(result);
+//     console.log(foo);
+// };
+// mumbler("abc");
+
+
+// const weirdStrInGcAse = (str) => {
+//     str = [...str];
+//     str = str.map((l, i) => i % 2 === 0 ? l.toUpperCase() : l.toLowerCase());
+//     str = str.toString().replace(/,/g, "");
+//     console.log(str);
+// }
+
+// weirdStrInGcAse("Arif Sanaullah Khan Baloch");
+
+// const stringIncrementer = str => {
+//   let strings = [];
+//   let nums = [];
+//   if (Number(str[str.length - 1])) {
+//     for (let i = 0; i < str.length; i++) {
+//       const element = str[i];
+//       if (Number(element)) {
+//         nums.push(element);
+//       } else {
+//         strings.push(element);
+//       }
+//     }
+//     nums = Number(String(nums).replace(/,/g, ""));
+//     nums++;
+//     str = strings.toString().replace(/,/g, "") + nums;
+//   } else {
+//     str = str + "1";
+//   }
+//   console.log(str);
+// };
+
+// stringIncrementer("Arif01");
+
+
+// const passwordValidator = password => {
+//     if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,})/.test(password)) {
+//         return true;
+//     }
+//     return false;
+// };
+
+// console.log(passwordValidator("Arif3"));
+// console.log(passwordValidator("Arif3a"));
+
+const numberOfTrailingZeros = (number) => {
+    const getFactorial = (number) => number ? number * getFactorial(number - 1) : 1;
+    let factorial = getFactorial(number);
+    console.log(factorial);
+    
 };
-mumbler("abc");
+numberOfTrailingZeros(12);
